4906e7bfb2d4f2f22cde51bcf4e6f9bdfe1e0fbf
kamalogudah <mcpaul2058@gmail.com>
2019-02-24 23:40:48 +0300

    Add Nested Component

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,19 @@
+
 <template>
-  <h1>Events Listing</h1>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard/>
+  </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
+
+<style scoped>
+</style>
