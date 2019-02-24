72bfcd6705007b8121451fb54ef53019f1c931f9
kamalogudah <mcpaul2058@gmail.com>
2019-02-24 13:27:49 +0300

    Optimizing your Editor

---
+++ src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+props: {
+    propName: {
+        type: Number,
+        default: 
+    },
+},
+}
+</script>
+
+<style>
+</style>
